0c49d2037559cde8b00a7afec6b4803870779881
sayedakhan <sayeda.khan97@gmail.com>
2022-01-03 18:04:39 -0500

    created test file for 507 final
    for streamlit

---
+++ streamlitex.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 17:59:38 2022
+
+@author: sayedakhan
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Virtual trainings hosted on Microsoft Teams')
